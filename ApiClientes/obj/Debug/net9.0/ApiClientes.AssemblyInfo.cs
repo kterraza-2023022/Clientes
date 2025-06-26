a13b5971a56cc0a3e923069ea56f33a56239f20d
kterraza-2023022 <kterraza-2023022@kinal.edu.gt>
2025-06-25 21:24:06 -0600

    actunose

--- ApiClientes/obj/Debug/net9.0/ApiClientes.AssemblyInfo.cs
+++ ApiClientes/obj/Debug/net9.0/ApiClientes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiClientes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+130fa7496ab7e7ba0854b359957eb3ac8640d8d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57128b1e89ea0552ea91e579d011b30535a44218")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiClientes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiClientes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
